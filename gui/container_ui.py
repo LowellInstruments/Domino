66767dd9204d9c090fe3335968146b2b0a386de1
Jeff Grant <jeffery.grant@gmail.com>
2018-10-12 11:18:26 -0400

    Initial GUI design

---
+++ gui/container_ui.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './designer_files/container.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setMinimumSize(QtCore.QSize(800, 600))
+        MainWindow.setMaximumSize(QtCore.QSize(800, 600))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setIconSize(QtCore.QSize(36, 36))
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.tab)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.frame_setup_file = QtWidgets.QFrame(self.tab)
+        self.frame_setup_file.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_setup_file.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_setup_file.setObjectName("frame_setup_file")
+        self.gridLayout_2.addWidget(self.frame_setup_file, 0, 0, 1, 1)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icons/icons/icons8-services-48.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.tabWidget.addTab(self.tab, icon, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.tab_2)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.frame_start_stop = QtWidgets.QFrame(self.tab_2)
+        self.frame_start_stop.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_start_stop.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_start_stop.setObjectName("frame_start_stop")
+        self.gridLayout_3.addWidget(self.frame_start_stop, 0, 0, 1, 1)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/icons/icons/usb.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.tabWidget.addTab(self.tab_2, icon1, "")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.tab_3)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.frame_convert = QtWidgets.QFrame(self.tab_3)
+        self.frame_convert.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_convert.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_convert.setObjectName("frame_convert")
+        self.gridLayout_4.addWidget(self.frame_convert, 0, 0, 1, 1)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/icons/icons/convert.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.tabWidget.addTab(self.tab_3, icon2, "")
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setSizeGripEnabled(False)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(1)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Lowell Instruments - Logger 0.1"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Create Setup File"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Start/Stop Device"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Convert Files"))
+
+import icons_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ gui/main.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtGui, QtWidgets, QtCore
+from gui.container_ui import Ui_MainWindow
+from gui.start_stop_ui import Ui_Frame as StartStopFrame
+from gui.converter_ui import Ui_Frame as ConverterFrame
+from gui.setup_screen_ui import Ui_Frame as SetupFrame
+import sys
+
+
+class Container(Ui_MainWindow):
+    def __init__(self, window):
+        self.window = window
+        self.setupUi(window)
+        self.converter_frame = ConverterFrame()
+        self.converter_frame.setupUi(self.frame_convert)
+        self.setup_frame = SetupFrame()
+        self.setup_frame.setupUi(self.frame_setup_file)
+        self.start_stop_frame = StartStopFrame()
+        self.start_stop_frame.setupUi(self.frame_start_stop)
+        self.pushButton1 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton1.setGeometry(QtCore.QRect(740, 5, 48, 48))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icons/icons/icons8-menu-48.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton1.setIcon(icon)
+        self.pushButton1.setIconSize(QtCore.QSize(36, 36))
+        self.pushButton1.setFlat(True)
+        self.pushButton1.setObjectName('Preferences')
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()  # create a main window
+    ui = Container(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
