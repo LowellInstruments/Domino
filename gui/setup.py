41a12179c4b63e3313a31693a355555bf5c2e845
Jeff Grant <jeffery.grant@gmail.com>
2018-11-05 15:20:44 -0500

    Connected enable check boxes, expanded mapping to reduce code, enable filename

--- gui/setup.py
+++ gui/setup.py
@@ -5,43 +5,87 @@ from setup_file.setup_file import (
     INTERVAL_STRING,
     BURST_FREQUENCY,
     TEMPERATURE_INTERVAL,
-    ORIENTATION_INTERVAL
+    ORIENTATION_INTERVAL,
+    ACCELEROMETER_ENABLED,
+    MAGNETOMETER_ENABLED,
+    TEMPERATURE_ENABLED,
+    LED_ENABLED
 )
 from re import search
+from collections import namedtuple
+
+
+sensor_map = namedtuple('sensor_map', ['widget', 'value', 'change_fcn'])
 
 
 class SetupFrame(Ui_Frame):
     def __init__(self):
         self.setup_file = SetupFile()
-        self.orient_map = None
-        self.temp_map = None
-        self.interval_widget = None
-        self.interval_change_fcn = None
-        self.interval_value = None
+        self.interval_mapping = None
+        self.sensor_mapping = None
 
     def setupUi(self, frame):
         super().setupUi(frame)
         self.populate_combo_boxes()
+        self.lineEdit_file_name.textChanged.connect(
+                    self.filename_changed)
         self.comboBox_orient_interval.currentIndexChanged.connect(
-                            lambda: self.interval_changed('orientation'))
+                    lambda: self.interval_changed('orientation'))
         self.comboBox_temp_interval.currentIndexChanged.connect(
-                            lambda: self.interval_changed('temperature'))
+                    lambda: self.interval_changed('temperature'))
+        self.checkBox_temperature.stateChanged.connect(
+                    lambda: self.sensor_enabled_slot('temperature'))
+        self.checkBox_magnetometer.stateChanged.connect(
+                    lambda: self.sensor_enabled_slot('magnetometer'))
+        self.checkBox_accelerometer.stateChanged.connect(
+                    lambda: self.sensor_enabled_slot('accelerometer'))
+        self.checkBox_led.stateChanged.connect(
+                    lambda: self.sensor_enabled_slot('led'))
         self.lineEdit_burst_duration.textChanged.connect(self.duration_changed)
         self.checkBox_continuous.stateChanged.connect(self.continuous_changed)
         self.comboBox_orient_burst_rate.currentIndexChanged.connect(
                                                 self.burst_rate_slot)
         self.lineEdit_file_name.setMaxLength(11)
-        self.interval_widget = \
-            {'temperature': self.comboBox_temp_interval,
-             'orientation': self.comboBox_orient_interval}
-        self.interval_change_fcn = \
-            {'temperature': self.setup_file.set_temperature_interval,
-             'orientation': self.setup_file.set_orient_interval}
-        self.interval_value = \
-            {'temperature': TEMPERATURE_INTERVAL,
-             'orientation': ORIENTATION_INTERVAL}
+        self.setup_mapping()
 
         self.redraw()
+
+    def setup_mapping(self):
+        self.interval_mapping = {
+            'temperature':
+                sensor_map(
+                    self.comboBox_temp_interval,
+                    lambda: self.setup_file.value(TEMPERATURE_INTERVAL),
+                    self.setup_file.set_temperature_interval),
+            'orientation':
+                sensor_map(
+                    self.comboBox_orient_interval,
+                    lambda: self.setup_file.value(ORIENTATION_INTERVAL),
+                    self.setup_file.set_orient_interval)
+        }
+
+        self.sensor_mapping = {
+            'temperature':
+                sensor_map(
+                    self.checkBox_temperature,
+                    lambda: self.setup_file.value(TEMPERATURE_ENABLED),
+                    self.setup_file.set_temperature_enabled),
+            'accelerometer':
+                sensor_map(
+                    self.checkBox_accelerometer,
+                    lambda: self.setup_file.value(ACCELEROMETER_ENABLED),
+                    self.setup_file.set_accelerometer_enabled),
+            'magnetometer':
+                sensor_map(
+                    self.checkBox_magnetometer,
+                    lambda: self.setup_file.value(MAGNETOMETER_ENABLED),
+                    self.setup_file.set_magnetometer_enabled),
+            'led':
+                sensor_map(
+                    self.checkBox_led,
+                    lambda: self.setup_file.value(LED_ENABLED),
+                    self.setup_file.set_led_enabled)
+        }
 
     def populate_combo_boxes(self):
         self.comboBox_temp_interval.addItems(INTERVAL_STRING)
@@ -51,15 +95,33 @@ class SetupFrame(Ui_Frame):
 
     def redraw(self):
         self.redraw_combo_boxes()
+        orient_group = [self.comboBox_orient_interval,
+                        self.comboBox_orient_burst_rate,
+                        self.lineEdit_burst_duration,
+                        self.checkBox_continuous]
+        if self.setup_file.orient_enabled():
+            self._set_enabled(orient_group, True)
+            self.continuous_changed()
+        else:
+            self._set_enabled(orient_group, False)
+
+    def filename_changed(self):
+        string = self.lineEdit_file_name.text()
+        try:
+            self.setup_file.set_filename(string + '.lid')
+            self.show_error(self.lineEdit_file_name, False)
+        except ValueError:
+            self.show_error(self.lineEdit_file_name, True)
 
     def redraw_combo_boxes(self):
         for sensor in ['temperature', 'orientation']:
             intervals = self.setup_file.available_intervals(sensor)
             for i, val in enumerate(intervals):
-                self.interval_widget[sensor].model().item(i).setEnabled(val)
-            interval = self.setup_file.value(self.interval_value[sensor])
+                widget = self.interval_mapping[sensor].widget
+                widget.model().item(i).setEnabled(val)
+            interval = self.interval_mapping[sensor].value()
             index = list(INTERVALS).index(interval)
-            self.interval_widget[sensor].setCurrentIndex(index)
+            self.interval_mapping[sensor].widget.setCurrentIndex(index)
 
     def burst_rate_slot(self):
         index = self.comboBox_orient_burst_rate.currentIndex()
@@ -67,30 +129,27 @@ class SetupFrame(Ui_Frame):
         self.setup_file.set_orient_burst_rate(burst_rate)
 
     def interval_changed(self, sensor):
-        index = self.interval_widget[sensor].currentIndex()
-        self.interval_change_fcn[sensor](INTERVALS[index])
+        index = self.interval_mapping[sensor].widget.currentIndex()
+        self.interval_mapping[sensor].change_fcn(INTERVALS[index])
         self.redraw_combo_boxes()
 
     def sensor_enabled_slot(self, sensor):
-        pass
+        state = self.sensor_mapping[sensor].widget.isChecked()
+        self.sensor_mapping[sensor].change_fcn(state)
+        self.redraw()
 
     def duration_changed(self):
         seconds = self.lineEdit_burst_duration.text()
         rate = BURST_FREQUENCY[self.comboBox_orient_burst_rate.currentIndex()]
-        if not self._verify_integer(seconds):
-            self._error_color(self.lineEdit_burst_duration, True)
+        if not search('^[1-9]+[0-9]*$', seconds):
+            self.show_error(self.lineEdit_burst_duration, True)
         try:
             self.setup_file.set_orient_burst_count(int(seconds)*rate)
-            self._error_color(self.lineEdit_burst_duration, False)
+            self.show_error(self.lineEdit_burst_duration, False)
         except ValueError:
-            self._error_color(self.lineEdit_burst_duration, True)
+            self.show_error(self.lineEdit_burst_duration, True)
 
-    def _verify_integer(self, string):
-        if search('^[1-9]+[0-9]*$', string):
-            return True
-        return False
-
-    def _error_color(self, widget, error):
+    def show_error(self, widget, error):
         if error:
             widget.setStyleSheet('background-color: rgb(255, 255, 0);')
         else:
