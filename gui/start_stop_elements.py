dad1ad7fd2d12e2cd500e8c8c6fd6ec8ab023501
Jeff Grant <jeffery.grant@gmail.com>
2018-11-27 09:59:44 -0500

    implements real time sensor data table

--- gui/start_stop_elements.py
+++ gui/start_stop_elements.py
@@ -1,7 +1,20 @@
 from yaml import load
 from gui.sensor_formats import hundredths_format, thousands_format, int_format
 from re import search
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
+from numpy import ndarray
+from collections import OrderedDict
+
+
+GUI_SENSOR_INFO = OrderedDict(
+    [('ax', thousands_format),
+     ('ay', thousands_format),
+     ('az', thousands_format),
+     ('mx', int_format),
+     ('my', int_format),
+     ('mz', int_format),
+     ('temp', thousands_format),
+     ('batt', hundredths_format)])
 
 
 def build_commands(path, gui):
@@ -43,11 +56,11 @@ class Status:
         self.gui = gui
         self.rabbit_icon = QtGui.QIcon()
         self.rabbit_icon.addPixmap(
-            QtGui.QPixmap(":/icons/icons/icons8-running-rabbit-48.png"),
+            QtGui.QPixmap(':/icons/icons/icons8-running-rabbit-48.png'),
             QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.stopped_icon = QtGui.QIcon()
         self.stopped_icon.addPixmap(
-            QtGui.QPixmap(":/icons/icons/icons8-private-48.png"),
+            QtGui.QPixmap(':/icons/icons/icons8-private-48.png'),
             QtGui.QIcon.Normal, QtGui.QIcon.Off)
 
     def update(self, data):
@@ -63,3 +76,33 @@ class Status:
         icon = self.rabbit_icon if state is True else self.stopped_icon
         self.gui.pushButton_icon.setIcon(icon)
         self.gui.pushButton_icon.setIconSize(QtCore.QSize(36, 36))
+
+
+class Sensors:
+    def __init__(self, gui):
+        self.gui = gui
+
+    def update(self, data):
+        for index, sensor in enumerate(GUI_SENSOR_INFO.keys()):
+            self._set_item_text(index, 1, self.enabled_string(sensor, data))
+            self._set_item_text(index, 2, self.value_string(sensor, data))
+
+    def _set_item_text(self, row, col, value):
+        item = QtWidgets.QTableWidgetItem(value)
+        self.gui.tableWidget.setItem(row, col, item)
+
+    def enabled_string(self, sensor, readings):
+        if readings and sensor in readings:
+            return 'Yes'
+        return 'No'
+
+    def value_string(self, sensor, readings):
+        if not readings:
+            return ''
+        reading = readings.get(sensor, '')
+        if isinstance(reading, ndarray):
+            return self._format_sensor_value(sensor, reading[0])
+        return self._format_sensor_value(sensor, reading)
+
+    def _format_sensor_value(self, sensor, value):
+        return GUI_SENSOR_INFO[sensor](value)
