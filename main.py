dd1bf7528b055fea4d92433d128c39214b66a31b
Jeff Grant <jeffery.grant@gmail.com>
2022-08-05 15:24:28 -0400

    remove changes from existing commit on branch (forgot to check if branch was empty)

--- main.py
+++ main.py
@@ -6,29 +6,7 @@ from PyQt5 import QtCore
 from gui.container import Container
 import sys
 from gui._version import __version__
-import win32gui
 
-
-def window_enum_handler(hwnd, resultList):
-    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '':
-        resultList.append((hwnd, win32gui.GetWindowText(hwnd)))
-
-
-def get_app_list(handles=[]):
-    mlst=[]
-    win32gui.EnumWindows(window_enum_handler, handles)
-    for handle in handles:
-        mlst.append(handle)
-    return mlst
-
-
-for handle, name in get_app_list():
-    if 'Lowell Instruments - Domino' in name:
-        win32gui.ShowWindow(handle, 9)
-        # win32gui.ShowWindow(handle, 5)
-        win32gui.BringWindowToTop(handle)
-        win32gui.RedrawWindow(handle)
-        sys.exit()
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
